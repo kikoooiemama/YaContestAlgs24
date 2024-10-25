7c49e62d99769669692291ec6311fbbeab4aa526
pakhomov <pakhomov@anispro.ru>
2024-10-25 18:02:10 +0300

    01C prepare

--- Contest_01/1C/01C.py
+++ Contest_01/1C/01C.py
@@ -1,1 +1,11 @@
 # Created by Nikolay Pakhomov 24.10.2024
+# I, O, C, L, H, P, X
+
+def find_letter():
+    pass
+
+
+table = []
+with open('input4.txt') as f:
+    for i in range(8):
+        desk.append(f.readline().strip())
---
+++ Contest_01/1C/01C_final.py
@@ -0,0 +1,1 @@
+# Created by Nikolay Pakhomov 25.10.2024
