cac9f49bc316b427dd777b03e42b6ea26592d192
kikoooiemama <jojoka17@yandex.ru>
2024-10-30 02:54:47 +0300

    01 sample

--- Contest_01/1A/01A_sample.py
+++ Contest_01/1A/01A_sample.py
@@ -1,1 +1,15 @@
 # Created by Nikolay Pakhomov 27.10.2024
+# Идея такая: рассматриваем не 8 случаев, а 4.
+with open('input.txt') as f:
+    x1, y1, x2, y2, x, y = list(map(int, f.readlines()))
+
+ans = ""
+if y > y2:
+    ans += "N"
+if y < y1:
+    ans += "S"
+if x < x1:
+    ans += "W"
+if x > x2:
+    ans += "E"
+print(ans)
