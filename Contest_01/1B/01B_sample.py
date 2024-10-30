79f0ec2b8a1f7972f5d6619e5cd9de1be4b702f3
kikoooiemama <jojoka17@yandex.ru>
2024-10-30 03:02:06 +0300

    01b sample

--- Contest_01/1B/01B_sample.py
+++ Contest_01/1B/01B_sample.py
@@ -1,1 +1,21 @@
 # Created by Nikolay Pakhomov 27.10.2024
+# 4 способа вытаскивать вещи!!! Мы выбираем тот, где сумма N+M минимальна!
+# 1. Синий: B + 1, D + 1
+# 2. Красный: A + 1, C + 1
+# 3. Майки обоих, любые носки: max(A, B) + 1, 1
+# 4. Носки обоих цветов и любая майка: 1, max(C,D) + 1
+
+with open('input.txt') as f:
+    a, b, c, d = list(map(int, f.readlines()))
+
+ans = []
+if a > 0 and c > 0:
+    ans.append([b + 1, d + 1])
+if b > 0 and d > 0:
+    ans.append([a + 1, c + 1])
+if a > 0 and b > 0:
+    ans.append([max(a, b) + 1, 1])
+if c > 0 and d > 0:
+    ans.append([1, max(c, d) + 1])
+m = min(ans, key=sum)
+print(*m)
