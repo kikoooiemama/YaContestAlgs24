4b02835e6323800898c02d7f938f68aff81b81f5
kikoooiemama <jojoka17@yandex.ru>
2024-11-01 00:04:10 +0300

    02A is done!

--- Contest_02/1A/01A.py
+++ Contest_02/1A/01A.py
@@ -1,1 +1,15 @@
 # Created by Nikolay Pakhomov 27.10.2024
+with open("input.txt") as f:
+    n = int(f.readline().strip())
+    a = list(map(int, f.readline().split()))
+
+s = 0
+result = []
+for i in range(n):
+    s += a[i]
+    result.append(s)
+print(result)
+
+with open("output.txt") as f:
+    b = list(map(int, f.readline().split()))
+print(result == b)
--- Contest_02/1A/01A_final.py
+++ Contest_02/1A/01A_final.py
@@ -1,1 +1,11 @@
 # Created by Nikolay Pakhomov 27.10.2024
+with open("input.txt") as f:
+    n = int(f.readline().strip())
+    a = list(map(int, f.readline().split()))
+
+s = 0
+result = []
+for i in range(n):
+    s += a[i]
+    result.append(s)
+print(" ".join(map(str, result)))
