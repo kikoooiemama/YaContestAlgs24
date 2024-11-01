39adb00604e054f9d9b5036c4d07e5738acb7b39
pakhomov <pakhomov@anispro.ru>
2024-11-01 19:40:52 +0300

    02F prepare

--- Contest_02/2F/02F.py
+++ Contest_02/2F/02F.py
@@ -1,1 +1,26 @@
 # Created by Nikolay Pakhomov 27.10.2024
+# Тупой алгоритм
+def get_prefix_sum(sequence, mod):
+    prefixsum = [0] * len(sequence + 1)
+    for i in range(1, len(sequence) + 1):
+        prefixsum[i] = prefixsum[i - 1] + sequence[i - 1]
+
+
+def find_number_by_mod(n, a: list):
+    i, j, k = 0
+    r = 0
+    for i in range(n - 2):
+        for j in range(i + 1, n - 1):
+            for k in range(j + 1, n):
+                pass
+    return r
+
+
+with open("input.txt") as f:
+    nn = int(f.readline().strip())
+    al = list(map(int, f.readline().split()))
+
+# Сортировка O(n*log n)
+al.sort()
+ans = find_number_by_mod(nn, al)
+print(ans)
---
+++ Contest_02/2F/02F_time.py
@@ -0,0 +1,9 @@
+# Created by Nikolay Pakhomov 01.11.2024
+import timeit
+
+code_to_test = """
+
+"""
+# вычисление времени выполнения кода
+elapsed_time = timeit.timeit(code_to_test, number=100) / 100
+print('Elapsed time: ', elapsed_time)
