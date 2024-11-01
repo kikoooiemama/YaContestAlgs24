7dfb7b899a328af307ba4fb8eb5efbdbf465da50
pakhomov <pakhomov@anispro.ru>
2024-11-01 16:44:27 +0300

    02E is done!

--- Contest_02/2E/02E.py
+++ Contest_02/2E/02E.py
@@ -1,1 +1,30 @@
 # Created by Nikolay Pakhomov 27.10.2024
+def get_median_idx(n):
+    if n % 2 == 0:
+        return n // 2 - 1
+    else:
+        return n // 2
+
+
+def calculate_seq(n, a):
+    """
+        Нужно найти последовательность медиан. Находим 1 - удаляем, находим следующую - удаляем и так пока размер
+        последовательности не дойдет до 0.
+    """
+    result = ""
+    for i in range(n):
+        idx = get_median_idx(len(a))
+        result += f" {a[idx]}"
+        # result.append(a[idx])
+        a.pop(idx)
+    return result[1:]
+
+
+with open("input.txt") as f:
+    nn = int(f.readline().strip())
+    al = list(map(int, f.readline().split()))
+
+# Сортировка O(n*log n)
+al.sort()
+ans = calculate_seq(nn, al)
+print(ans)
--- Contest_02/2E/02E_time.py
+++ Contest_02/2E/02E_time.py
@@ -1,4 +1,7 @@
-# Created by Nikolay Pakhomov 27.10.2024
+# Created by Nikolay Pakhomov 01.11.2024
+import timeit
+
+code_to_test = """
 def get_median_idx(n):
     if n % 2 == 0:
         return n // 2 - 1
@@ -15,10 +18,14 @@ def calculate_seq(n, a):
     return result[1:]
 
 
-with open("input.txt") as f:
+with open("input_time.txt") as f:
     nn = int(f.readline().strip())
     al = list(map(int, f.readline().split()))
 
 al.sort()
 ans = calculate_seq(nn, al)
 print(ans)
+"""
+# вычисление времени выполнения кода
+elapsed_time = timeit.timeit(code_to_test, number=100) / 100
+print('Elapsed time: ', elapsed_time)
