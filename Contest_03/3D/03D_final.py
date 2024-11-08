d89d4d8e73380492efb5b03f0dcdef38c5069d1d
pakhomov <pakhomov@anispro.ru>
2024-11-08 14:27:56 +0300

    03D is done!

--- Contest_03/3D/03D_final.py
+++ Contest_03/3D/03D_final.py
@@ -1,1 +1,16 @@
 # Created by Nikolay Pakhomov 04.11.2024
+with open("input.txt") as f:
+    postfix = f.readline().strip().split()
+
+stack = []
+for el in postfix:
+    a, b = 0, 0
+    if el == "+":
+        stack.append(stack.pop(-1) + stack.pop(-1))
+    elif el == "*":
+        stack.append(stack.pop(-1) * stack.pop(-1))
+    elif el == "-":
+        stack.append(-(stack.pop(-1) - stack.pop(-1)))
+    else:
+        stack.append(int(el))
+print(stack[0])
