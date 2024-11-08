25ff1c74a7bcb4ee6a2fe6b6b7224bb20f17218c
pakhomov <pakhomov@anispro.ru>
2024-11-08 11:06:10 +0300

    03A is done!

--- Contest_03/3A/03A_final.py
+++ Contest_03/3A/03A_final.py
@@ -1,1 +1,26 @@
-# Created by Nikolay Pakhomov 04.11.2024
+# Created by Nikolay Pakhomov 08.11.2024
+def check_seq(seq):
+    brackets = {"(": ")", "[": "]", "{": "}"}
+    stack = []
+    for bracket in seq:
+        if bracket in brackets.keys():
+            stack.append(bracket)
+        else:
+            if len(stack) == 0:
+                return "no"
+            if stack[-1] in brackets.keys():
+                if brackets.get(stack[-1]) == bracket:
+                    stack.pop(-1)
+                else:
+                    return "no"
+            else:
+                return "no"
+    if len(stack) != 0:
+        return "no"
+    return "yes"
+
+
+with open("input.txt") as f:
+    string = f.readline().strip()
+
+print(check_seq(string))
