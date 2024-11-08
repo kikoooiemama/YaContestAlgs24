bc7908199d909a7873d307f5cd49d76b7ccc8e83
pakhomov <pakhomov@anispro.ru>
2024-11-08 11:41:25 +0300

    03B is done!

--- Contest_03/3B/03B.py
+++
@@ -1,1 +0,0 @@
-# Created by Nikolay Pakhomov 04.11.2024
--- Contest_03/3B/03B_final.py
+++ Contest_03/3B/03B_final.py
@@ -1,1 +1,22 @@
-# Created by Nikolay Pakhomov 04.11.2024
+# Created by Nikolay Pakhomov 08.11.2024
+# Задача на поиск ближайшего меньшего справа. Используем стеки.
+with open("input.txt") as f:
+    n = int(f.readline())
+    a = list(map(int, f.readline().split()))
+ans = [-1] * n
+stack = []
+for i in range(0, n):
+    if len(stack) == 0 or a[i - 1] < a[i]:
+        stack.append((a[i], i))
+        continue
+    else:
+        for j in range(len(stack) - 1, -1, -1):
+            if stack[j][0] > a[i]:
+                ans[stack[j][1]] = i
+                stack.pop(-1)
+                continue
+            else:
+                break
+        stack.append((a[i], i))
+
+print(*ans)
