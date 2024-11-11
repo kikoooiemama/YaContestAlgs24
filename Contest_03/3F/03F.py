1f3e7b4a4c83b85b4c078a26c7a529ccb011ecb6
pakhomov <pakhomov@anispro.ru>
2024-11-11 18:52:21 +0300

    03F prep

--- Contest_03/3F/03F.py
+++ Contest_03/3F/03F.py
@@ -1,1 +1,9 @@
 # Created by Nikolay Pakhomov 04.11.2024
+with open("input.txt") as f:
+    n = int(f.readline().strip())
+    order = f.readline()
+    s = f.readline()
+
+count_sq = 0
+count_ci = 0
+# Если у строк A, B одинаковые подстроки s, то если A_s+1 < B_s+1, то A < B.
