114a79078f26ba5d8ade22cfc84d43f3f4e9dc45
pakhomov <pakhomov@anispro.ru>
2024-11-12 10:58:17 +0300

    03H is done!

--- Contest_03/3H/03H_final.py
+++ Contest_03/3H/03H_final.py
@@ -1,1 +1,21 @@
 # Created by Nikolay Pakhomov 04.11.2024
+def rsq(p_sum, left, right):
+    return p_sum[right] - p_sum[left]
+
+
+prefix_sum = [0]
+stack = []
+with open("input.txt") as f:
+    n = int(f.readline().strip())
+    for i in range(n):
+        operation = f.readline().strip()
+        if operation.startswith("+"):
+            x = int(operation)
+            stack.append(x)
+            prefix_sum.append(x + prefix_sum[-1])
+        elif operation.startswith("?"):
+            k = int(operation[1:])
+            print(rsq(prefix_sum, len(stack) - k, len(stack)))
+        else:
+            prefix_sum.pop(-1)
+            print(stack.pop(-1))
--- Contest_03/3H/03H_time.py
+++ Contest_03/3H/03H_time.py
@@ -1,11 +1,14 @@
-# Created by Nikolay Pakhomov 04.11.2024
+# Created by Nikolay Pakhomov 12.11.2024
+import timeit
+
+code_to_test = """
 def rsq(p_sum, left, right):
     return p_sum[right] - p_sum[left]
 
 
 prefix_sum = [0]
 stack = []
-with open("input.txt") as f:
+with open("input_x.txt") as f:
     n = int(f.readline().strip())
     for i in range(n):
         operation = f.readline().strip()
@@ -19,3 +22,9 @@ with open("input.txt") as f:
         else:
             prefix_sum.pop(-1)
             print(stack.pop(-1))
+
+
+"""
+# вычисление времени выполнения кода
+elapsed_time = timeit.timeit(code_to_test, number=100) / 100
+print('Elapsed time: ', elapsed_time)
