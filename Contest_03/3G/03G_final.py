30d834c387f9102308fbf62121a6e7de2a98037a
kikoooiemama <jojoka17@yandex.ru>
2024-11-12 03:09:06 +0300

    03G is done!

--- Contest_03/3G/03G_final.py
+++ Contest_03/3G/03G_final.py
@@ -1,1 +1,17 @@
 # Created by Nikolay Pakhomov 04.11.2024
+with open("input.txt") as f:
+    n, b = map(int, f.readline().split())
+    a = list(map(int, f.readline().split()))
+sum_time = a[0]
+clients = a[0]
+for i in range(1, n):
+    clients -= b
+    if clients < 0:
+        clients = 0
+    clients += a[i]
+    sum_time += clients
+clients -= b
+if clients < 0:
+    clients = 0
+sum_time += clients
+print(sum_time)
--- Contest_03/3G/03G_time.py
+++ Contest_03/3G/03G_time.py
@@ -1,9 +1,10 @@
-# Created by Nikolay Pakhomov 04.11.2024
-with open("input.txt") as f:
+# Created by Nikolay Pakhomov 12.11.2024
+import timeit
+
+code_to_test = """
+with open("input_time.txt") as f:
     n, b = map(int, f.readline().split())
     a = list(map(int, f.readline().split()))
-
-print(n == len(a))
 sum_time = a[0]
 clients = a[0]
 for i in range(1, n):
@@ -18,4 +19,7 @@ if clients < 0:
 sum_time += clients
 print(sum_time)
 
-
+"""
+# вычисление времени выполнения кода
+elapsed_time = timeit.timeit(code_to_test, number=100) / 100
+print('Elapsed time: ', elapsed_time)
