eeb7e6afa1559d4622161291ac98ecd0278d3a89
pakhomov <pakhomov@anispro.ru>
2024-11-20 18:42:49 +0300

    04D prep

--- Contest_04/4D/04D.py
+++ Contest_04/4D/04D.py
@@ -1,1 +1,27 @@
 # Created by Nikolay Pakhomov 13.11.2024
+def print_tree(node):
+
+    left = print_tree()
+    print(left)
+    right = print_tree
+
+
+with open("input.txt") as f:
+    requests = [string.strip() for string in f.readlines()]
+print(requests)
+tree = {}
+for req in requests:
+    if req.startswith("A"):
+        v = int(req.split()[1])
+        if tree.get(v, None) is None:
+            print("ALREADY")
+            continue
+        if not len(tree):
+            tree[v] = [None, [None, None], 0]
+            continue
+        print(v)
+    elif req.startswith("S"):
+        v = int(req.split()[1])
+        print("YES") if tree.get(v, None) is None else print("NO")
+    else:
+        print_tree()
