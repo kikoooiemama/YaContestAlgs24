d819a5e45965e8cce41945ec4132400ce944a505
pakhomov <pakhomov@anispro.ru>
2024-11-21 10:39:04 +0300

    04E is done!

--- Contest_04/4E/04E.py
+++ Contest_04/4E/04E.py
@@ -2,6 +2,9 @@
 # Неориентированное дерево (или просто дерево) – это конечный связный граф с выделенной вершиной (корнем) без циклов.
 # Дерево не имеет петель и кратных рёбер.
 # Вершина — точка, в которой две кривые, две прямые либо два ребра сходятся
+import sys
+
+
 def calculate_subtree_size(node):
     dad = tree[node][0]
     sons = tree[node][1]
@@ -12,21 +15,38 @@ def calculate_subtree_size(node):
         tree[dad][2] = tree[dad][2] + tree[node][2]
 
 
+def find_dad(node):
+    sons = tree[node][1]
+    for son in sons:
+        tree[son][0] = node
+        tree[son][1].discard(node)
+        find_dad(son)
+
+
+sys.setrecursionlimit(1000000000)
 # dad, sons, sub_size. dict - в 3.12 питоне упорядочены ключи
-tree = {1: [None, [], 0]}
-with open("input2.txt") as f:
+tree = {1: [None, set(), 0]}
+with open("input.txt") as f:
     v = int(f.readline().strip())
     for i in range(v - 1):
-        # одна из вершин новая - ее нужно добавить
         top_1, top_2 = map(int, f.readline().split())
-        if tree.get(top_1, None) is None:
-            tree[top_1] = [top_2, [], 0]
-            tree[top_2][1].append(top_1)
+        if tree.get(top_1, None) is None and tree.get(top_2, None) is not None:
+            tree[top_1] = [None, {top_2}, 0]
+            tree[top_2][1].add(top_1)
+        elif tree.get(top_2, None) is None and tree.get(top_1, None) is not None:
+            tree[top_2] = [None, {top_1}, 0]
+            tree[top_1][1].add(top_2)
+        elif tree.get(top_1, None) is None and tree.get(top_2, None) is None:
+            tree[top_1] = [None, {top_2}, 0]
+            tree[top_2] = [None, {top_1}, 0]
         else:
-            tree[top_2] = [top_1, [], 0]
-            tree[top_1][1].append(top_2)
+            tree[top_1][1].add(top_2)
+            tree[top_2][1].add(top_1)
 
+# пройтись по дереву и проставить sons/dads
+find_dad(1)
+# рекурсивно считаем количество вершин в поддеревьях
 calculate_subtree_size(1)
 print(f"{tree.pop(1)[2]}", end="")
-for k in tree.keys():
+for k in sorted(tree.keys()):
     print(f" {tree.get(k)[2]}", end="")
