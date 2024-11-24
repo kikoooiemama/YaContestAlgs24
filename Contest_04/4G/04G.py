535fb238e509e70ef16991563b9986cc3d0f42cd
kikoooiemama <jojoka17@yandex.ru>
2024-11-24 15:04:05 +0300

    04G tests

--- Contest_04/4G/04G.py
+++ Contest_04/4G/04G.py
@@ -8,7 +8,8 @@
 # Решения нет:
 # - 2 дятла не могут иметь > 1 общих знакомых
 # - 2 дятла, являющиеся знакомыми дятла Х, не могут быть знакомыми
-# - 2 дятла, являющиеся знакомыми дятла Х, могут иметь только еще 1 знакомого, у которого есть знакомые
+# - 2 дятла, являющиеся знакомыми дятла Х, могут иметь только еще 1 знакомого, у которого есть знакомые.
+# - У дятла есть 3 знакомых, у которых по 1.
 
 # Если дятла 2 и они имеют 2х общих знакомых - ответа нет, т.к. это пересечение.
 # У 2х дятлов может быть только 1 общий знакомый.
@@ -27,6 +28,8 @@
 # ((SUM b_size_i) + 2)^s * (П 2*(sub_b_size_i!)) * b!  ///(SUM, П от 1 до b)
 # Branch == b, Lines == l, Solos == s:
 # (П 2*(sub_b_size_i!)) * (b + l)! * 2^l * (2l + (SUM b_size_i) + 2)^s - вот это верно скорее всего
+# (2^b * П (sub_b_size_i!)) * (b + l)! * 2^l * (2l + (SUM b_size_i) + 2)^s
+# П (sub_b_size_i!) * (b + l)! * 2^(l+b) * (2l + (SUM b_size_i) + 2)^s
 
 # not only solos: (2^(b + l) * П (sub_b_size_i!)) * (b + l)! * (2l + (SUM b_size_i) + 2)^s  - обобщенная формула
 # only solos, n==s: (s+1)!
@@ -34,6 +37,7 @@
 # Trees == 1, t = 1:
 # В tree может быть и subtree
 
+# Вычисление факториала по модулю
 def mod_fact(r, mod):
     result = j = 1
     while j < r + 1:
@@ -41,22 +45,45 @@ def mod_fact(r, mod):
         j += 1
 
 
-sort_tree = {}
+# Считаем все комбинации для дерева
+def calculate_for_tree():
+    pass
+
+
+# Считаем комбинации для всех дятлов
+def calculate_combinations(n, mod, edges):
+    pairs = {}
+    branches = {}
+    forest = {}
+    structures = {}
+    singles = set()
+    for j in range(1, n + 1):
+        singles.add(j)
+    for el in edges:
+        a, b = el[0], el[1]
+        # dads, sons
+        if structures.get(a, None) is None and structures.get(b, None) is not None:
+            structures[a] = [b, set(), 0]
+            structures[b][1].add(a)
+        elif structures.get(b, None) is None and structures.get(a, None) is not None:
+            structures[b] = [{a}, {a}, 0]
+            structures[a][0].add(b)
+            structures[a][1].add(b)
+        elif structures.get(a, None) is None and structures.get(b, None) is None:
+            structures[a] = [{b}, {b}, 0]
+            structures[b] = [{a}, {a}, 0]
+        else:
+            structures[a][0].add(b)
+            structures[a][1].add(b)
+            structures[b][0].add(a)
+            structures[b][1].add(a)
+
+
+a_b = []
 with open("input.txt") as f:
     # 1 <= n <= 10^6, 1 <= M <= 10^7 - количество пар знакомых дятлов, 1 <= k <= 2*10^6
-    n, m, k = map(int, f.readline().split())
-    for i in range(1, n + 1):
-        sort_tree[i] = []
+    amount, m, k = map(int, f.readline().split())
     for i in range(m):
-        a, b = map(int, f.readline().split())
-        sort_tree[a].append(b)
-        sort_tree[b].append(a)
-
-singles = {}
-pairs = {}
-branches = {}
-trees = {}
-# Нужно научиться заполнять эти коллекции и следить за сложностью вычислений.
-
-print(n, m, k)
-print(sort_tree)
+        first, second = map(int, f.readline().split())
+        a_b.append((first, second))
+print(calculate_combinations(amount, k, a_b))
--- Contest_04/4G/04G_test.py
+++ Contest_04/4G/04G_test.py
@@ -1,12 +1,30 @@
 # Created by Nikolay Pakhomov 21.11.2024
+def calculate_combinations(n, mod, edges):
+    result = 0
+    return result
 
 
+n_list = []
+m_list = []
+k_list = []
+result_list = []
+a_b_list = []
+test_n = 10
+for i in range(2, test_n + 2):
+    with open(f"input{i}.txt") as f:
+        amount, m, k = map(int, f.readline().split())
+        n_list.append(amount)
+        m_list.append(m)
+        k_list.append(k)
+        a_b = []
+        for m_i in range(m):
+            first, second = map(int, f.readline().split())
+            a_b.append((first, second))
+        a_b_list.append(a_b)
 
+    with open(f"output{i}.txt") as f:
+        result_list.append(int(f.readline().strip()))
 
-
-
-
-
-
-
-
+for ii in range(test_n):
+    ans = calculate_combinations(n_list[ii], k_list[ii], a_b_list[ii])
+    print(f"Result: {ans}, Answer: {result_list[ii]}, Right: {ans == result_list[ii]}")
